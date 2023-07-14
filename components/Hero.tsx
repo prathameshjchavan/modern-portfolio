4e15468c3021df6a7d5806fe82349b419323c008
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-07-14 17:13:49 +0530

    hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,6 +2,8 @@
 
 import { Cursor, useTypewriter } from "react-simple-typewriter";
 import BackgroundCircles from "./BackgroundCircles";
+import Image from "next/image";
+import Link from "next/link";
 
 const Hero = () => {
 	const [text, count] = useTypewriter({
@@ -17,10 +19,41 @@ const Hero = () => {
 	return (
 		<div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
 			<BackgroundCircles />
-			<h1>
-				<span>{text}</span>
-				<Cursor cursorColor="#F7AB0A" />
-			</h1>
+			<Image
+				className="relative rounded-full h-32 w-32 mx-auto object-cover"
+				height={600}
+				width={600}
+				src="https://cdn.sanity.io/images/ltuexkre/production/a766c574c295f2103c63c2dfdf675557c664d016-600x600.png"
+				alt=""
+			/>
+
+			<div className="z-20">
+				<h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">
+					Software Engineer
+				</h2>
+
+				<h1>
+					<span className="mr-3 text-5xl xl:text-6xl font-semibold px-10">
+						{text}
+					</span>
+					<Cursor cursorColor="#F7AB0A" />
+				</h1>
+
+				<div className="pt-5">
+					<Link href="#about">
+						<button className="heroButton">About</button>
+					</Link>
+					<Link href="#experience">
+						<button className="heroButton">Experience</button>
+					</Link>
+					<Link href="#skills">
+						<button className="heroButton">Skills</button>
+					</Link>
+					<Link href="#projects">
+						<button className="heroButton">Projects</button>
+					</Link>
+				</div>
+			</div>
 		</div>
 	);
 };
