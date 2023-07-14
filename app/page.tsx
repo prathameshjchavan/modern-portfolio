31e68782bb4e2f9953f2fe741f7d346869c32f83
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-07-14 17:49:57 +0530

    fixed snapping issue

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,21 @@
+import About from "@/components/About";
+import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 
 export default function Home() {
 	return (
-		<main>
+		<main className="snap-y snap-mandatory h-screen overflow-y-scroll">
+			<Header />
+
 			{/* Hero */}
-			<section id="hero" className="snap-center">
+			<section id="hero" className="snap-start">
 				<Hero />
 			</section>
 
 			{/* About */}
+			<section id="about" className="snap-center">
+				<About />
+			</section>
 
 			{/* Experience */}
 
