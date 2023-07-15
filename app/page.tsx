92412ce8587152185a605a860366d5f6f107a60a
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-07-15 16:13:03 +0530

    projects section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import About from "@/components/About";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
+import Projects from "@/components/Projects";
 import Skills from "@/components/Skills";
 import WorkExperience from "@/components/WorkExperience";
 
@@ -26,6 +27,9 @@ export default function Home() {
 			</section>
 
 			{/* Projects */}
+			<section id="projects" className="snap-start">
+				<Projects />
+			</section>
 
 			{/* Contact Me */}
 		</main>
