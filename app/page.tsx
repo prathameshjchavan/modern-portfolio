990513bc8bf09b47b63c9b70af74522c654a8456
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-07-16 10:29:38 +0530

    home button

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import Hero from "@/components/Hero";
 import Projects from "@/components/Projects";
 import Skills from "@/components/Skills";
 import WorkExperience from "@/components/WorkExperience";
+import Link from "next/link";
 
 export default function Home() {
 	return (
@@ -36,6 +37,18 @@ export default function Home() {
 			<section id="contact" className="snap-start">
 				<ContactMe />
 			</section>
+
+			<Link href="#hero">
+				<footer className="sticky bottom-5 w-full cursor-pointer">
+					<div className="flex items-center justify-center">
+						<img
+							className="h-10 w-10 rounded-full filter grayscale hover:grayscale-0 cursor-pointer"
+							src="https://i.imgur.com/e2yvD6A.png"
+							alt=""
+						/>
+					</div>
+				</footer>
+			</Link>
 		</main>
 	);
 }
