309077b6967e11b5086c74137776fcbd88b91505
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-08-06 14:29:10 +0530

    about - mobile responsiveness

--- components/About.tsx
+++ components/About.tsx
@@ -15,7 +15,7 @@ const About = ({ pageInfo }: Props) => {
 			transition={{ duration: 1.5 }}
 			className="h-screen flex flex-col relative text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center"
 		>
-			<h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+			<h3 className="absolute top-20 sm:top-24 uppercase tracking-[20px] -mr-[20px] text-gray-500 text-2xl">
 				About
 			</h3>
 
@@ -25,16 +25,16 @@ const About = ({ pageInfo }: Props) => {
 				transition={{ duration: 1.2 }}
 				viewport={{ once: true }}
 				src={urlFor(pageInfo.profilePic).url()}
-				className="mt-20 -mb-20 md:mb-0 flex-shrink-0 w-40 h-40 rounded-full object-cover object-top md:rounded-lg md:w-64 md:h-96 xl:w-[400px] xl:h-[500px] xl:object-cover"
+				className="mt-10 sm:mt-20 -mb-20 md:mb-0 flex-shrink-0 w-40 h-40 rounded-full object-cover object-top md:rounded-lg md:w-64 md:h-96 xl:w-[400px] xl:h-[500px] xl:object-cover"
 			/>
 
-			<div className="space-y-10 px-0 md:px-10">
-				<h4 className="text-2xl sm:text-4xl font-semibold">
+			<div className="space-y-5 sm:space-y-10 px-0 md:px-10">
+				<h4 className="text-2xl sm:text-4xl font-semibold whitespace-nowrap">
 					Here is a{" "}
 					<span className="underline decoration-[#F7AB0A]/50">little</span>{" "}
 					background
 				</h4>
-				<p className="text-xs md:text-sm lg:text-base">
+				<p className="text-xs text-justify sm:text-center md:text-sm lg:text-base">
 					{pageInfo.backgroundInformation}
 				</p>
 			</div>
