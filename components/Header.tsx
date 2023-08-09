133a7ef2cf1355653bd827e643a2ff3431c6b5c0
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-08-09 13:45:39 +0530

    added header background

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,7 +9,7 @@ type Props = {
 
 const Header = ({ socials }: Props) => {
 	return (
-		<header className="sticky top-0 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center p-5">
+		<header className="sticky bg-[rgb(36,36,36)] top-0 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center p-5">
 			<motion.div
 				initial={{ x: -500, opacity: 0, scale: 0.5 }}
 				animate={{ x: 0, opacity: 1, scale: 1 }}
